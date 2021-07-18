05eebec3bd7bb49abd1b0e4d9beb4b39de17a121
Lena Hinrichsen <hinrichsen@hab.de>
2021-07-18 20:22:13 +0200

    added project names and deco

---
+++ font.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.1" name="font" tilewidth="512" tileheight="512" tilecount="1" columns="15">
+ <grid orientation="orthogonal" width="32" height="15"/>
+ <tile id="0">
+  <image width="512" height="512" source="../../../../Downloads/Sir_Henry's_32x32.png"/>
+ </tile>
+</tileset>
